e370cf50a00e5959dcc96da41c0b564b289ca2a4
Administrator <Administrator@TRAINING-D03>
2024-03-13 19:33:30 +0530

    New Commit Register and Login Together

---
+++ Project/Controllers/Students_newController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using Project.Models;
+using System.Configuration;
+using System.Data;
+
+
+namespace Project.Controllers
+{
+    public class Students_newController : Controller
+
+    {
+        string constring = ConfigurationManager.ConnectionStrings["dbcon"].ConnectionString;
+        // GET: Students_new
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration(Student user1)
+        {
+            var list = new List<string> { "HTML", "CSS", "JavaScript", "SQL", "ASP.NET" };
+            ViewBag.list = list;
+            try
+            {
+
+                using (SqlConnection connection = new SqlConnection(constring))
+                {
+                    SqlCommand command = new SqlCommand("insert into [dbo].[Students_New](student_id,first_name,last_name,address,gender,phone_no,course_name,password) values(@student_id, @first_name, @last_name, @address, @gender, @phone_no, @course_name,@password)", connection);
+
+
+                    command.Parameters.AddWithValue("@student_id", user1.student_id);
+                    command.Parameters.AddWithValue("@first_name", user1.first_name);
+                    command.Parameters.AddWithValue("@last_name", user1.last_name);
+                    command.Parameters.AddWithValue("@address", user1.address);
+                    command.Parameters.AddWithValue("@gender", user1.gender);
+                    command.Parameters.AddWithValue("@phone_no", user1.phone_no);
+                    command.Parameters.AddWithValue("@course_name", user1.course_name);
+                    command.Parameters.AddWithValue("@password", user1.password);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+    
+        public ActionResult Login(Login user1)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(constring))
+                {
+                    SqlCommand command = new SqlCommand("select [phone_no],[password] from [Students_New] where [phone_no]=@phone_no and [password]=@password", connection);
+
+
+                    command.Parameters.AddWithValue("@phone_no", user1.phone_no);
+                    command.Parameters.AddWithValue("@password", user1.password);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        Session["user_name"] = user1.phone_no.ToString();
+                        return RedirectToAction("welcome");
+                    }
+                    else
+                    {
+                        ViewData["Message"] = "User Login Details Failed";
+                    }
+
+
+                }
+
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+            public ActionResult welcome()
+            {
+                return View();
+            }
+
+        }
+
+ }
+
---
+++ Project/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models
+{
+    public class Login
+    { 
+        public string phone_no { get; set; }
+        public string password
+        {
+            get; set;
+        }
+    }
+}
